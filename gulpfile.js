ce19dd001250ecf7ae2610308b6a5f7eb149160b
Юлия <junova@yandex.ru>
2019-05-15 20:39:55 +0400

    уменьшила размер сайта до 2мб

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,11 @@ var gulp         = require('gulp'),
     minify       = require('gulp-csso'),
     gulpIf       = require('gulp-if'), 
     rename       = require('gulp-rename'),
-    svgSprite    = require('gulp-svg-sprite');
+    svgSprite    = require('gulp-svg-sprite'),
+    del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    cache        = require('gulp-cache'); // Подключаем библиотеку кеширования;
 
 gulp.task('less', function () {
    return gulp.src('app/less/main.less')
@@ -47,6 +51,45 @@ gulp.task('less', function () {
     .pipe(browserSync.reload({ stream: true }))
 });
 
+gulp.task('clean', async function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({ // С кешированием
+        // .pipe(imagemin({ // Сжимаем изображения без кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))/**/)
+        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+gulp.task('prebuild', async function() {
+
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+        'app/css/main.min.css',
+        'app/css/normalize.css'
+        ])
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function (callback) {
+    return cache.clearAll();
+})
+
     // для автоматической компиляции
 gulp.task('watch', function () {
 	gulp.watch('app/less/**/*.less', gulp.parallel('less'));
@@ -55,3 +98,4 @@ gulp.task('watch', function () {
 });
 // специальная задача, которая позволяет определить порядок обновления в браузере и автокомпилятор(watch)
 gulp.task('default', gulp.parallel('less','browser-sync','watch'));
+gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'less'));
